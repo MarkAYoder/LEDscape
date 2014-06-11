ea8dbd0809c5cad57775b3df852deb7065173ec9
Mark A. Yoder <Mark.A.Yoder@Rose-Hulman.edu>
2014-06-11 15:48:03 -0400

    Cleaned up unused code

--- may-clear.c
+++ may-clear.c
@@ -27,53 +27,31 @@ static void ledscape_fill_color(
 	}
 }
 
-int main (void)
+int main (int argc, char *argv[])
 {
 	const int num_pixels = 64; // 176;
 	ledscape_t * const leds = ledscape_init(num_pixels);
-	time_t last_time = time(NULL);
-	unsigned last_i = 0;
 
-	uint8_t rgb[3] = {4, 4, 4};
+	uint8_t rgb[3] = {0, 0, 0};
+	if(argc == 4) {
+	    for(int i=0; i<3; i++) {
+	        rgb[i] = atoi(argv[i+1]);
+	    }
+	}
 
-	unsigned i = 0;
+	unsigned frame_num = 0;
 
+	ledscape_frame_t * const frame = ledscape_frame(leds, frame_num);
 
-	const unsigned report_interval = 1;
-	unsigned last_report = 0;
-	unsigned long delta_sum = 0;
-	unsigned frames = 0;
+	ledscape_fill_color(
+		frame,
+		num_pixels,
+		rgb[0],
+		rgb[1],
+		rgb[2]
+	);
 
-		// Alternate frame buffers on each draw command
-		const unsigned frame_num = i++ % 2;
-
-		struct timeval start_tv, stop_tv, delta_tv;
-		gettimeofday(&start_tv, NULL);
-		ledscape_frame_t * const frame = ledscape_frame(leds, frame_num);
-
-		ledscape_fill_color(
-			frame,
-			num_pixels,
-			rgb[0],
-			rgb[1],
-			rgb[2]
-		);
-
-		// const uint32_t response = ledscape_wait(leds);
-		ledscape_draw(leds, frame_num);
-
-		gettimeofday(&stop_tv, NULL);
-		timersub(&stop_tv, &start_tv, &delta_tv);
-
-		frames++;
-		delta_sum += delta_tv.tv_usec;
-		if (stop_tv.tv_sec - last_report < report_interval)
-			// continue;
-		last_report = stop_tv.tv_sec;
-
-		frames = delta_sum = 0;
-
-	// ledscape_close(leds);
+	ledscape_draw(leds, frame_num);
 
 	return EXIT_SUCCESS;
 }
